4d63eacecbc0568d55adb8f7cabec184b01bdd4c
Vladi Vladi <vlast3k@gmail.com>
2016-05-16 06:12:45 +0300

    adding library for Cublic Sensors CM1102 and CM1106

--- CubicGasSensors/CubicGasSensors.cpp
+++ CubicGasSensors/CubicGasSensors.cpp
@@ -29,6 +29,7 @@ boolean CubicGasSensors::validateCS(byte* resp) {
     SERIAL << F("bad cs:") << _HEX(getCS(resp)) << F(", ") << _HEX(resp[resp[1]+2]) << endl;  
     return false;
   }
+  return true;
 }
 
 boolean CubicGasSensors::sendCmd(uint8_t *cmd, uint8_t *resp) {
@@ -57,22 +58,37 @@ void CubicGasSensors::co2Set400ppm() {
   
 }
 
-void CubicGasSensors::checkForReset() {
-  if (millis() > 5L*60*1000 && EEPROM.read(eepromReset) == 1) {
+void CubicGasSensors::setStatus(CubicStatus newStatus) {
+    if (currentStatus != newStatus) {
+        currentStatus = newStatus;
+        statusCb(currentStatus);
+    }
+}
+
+bool CubicGasSensors::checkForReset() {
+  if (EEPROM.read(eepromReset) != 1) return false;
+  setStatus(CB_CALIBRATE);
+  if (millis() > 5L*60*1000) {
     EEPROM.write(eepromReset, 0);
     #ifdef ESP8266
       EEPROM.commit();
     #endif
+    return false;
+  } else if (!sentResetCmd && millis() > 3L*60*1000) {
     co2Set400ppm();
+    sentResetCmd = true;
   }
+  SERIAL << F("Calibrating:") << millis()/1000 << F("s, ") << rawReadCM1106_CO2(DEBUG) << F("ppm\n");
+  return true;
 }
 
-int CubicGasSensors::rawReadCM1106_CO2() {
+int CubicGasSensors::rawReadCM1106_CO2(bool dbg) {
+  DEBUG = dbg;
   uint8_t cmdReadCO2[] = {4, 0x11, 0x01, 0x01, 0xed};
   uint8_t resp[30];
   sendCmd(cmdReadCO2, resp);
   uint16_t value = ((uint16_t)256)*resp[3] + resp[4];
-  if (DEBUG) SERIAL << value << endl;
+  if (DEBUG) SERIAL << "sss:" << value << endl;
   raCM1106.addValue(value);
   return value;  
 }
@@ -80,16 +96,19 @@ int CubicGasSensors::rawReadCM1106_CO2() {
 
 int CubicGasSensors::getCO2(boolean dbg) {
   DEBUG = dbg;
+  if (checkForReset()) return -1;
   if (timePassed(lastNDIRRead, NDIR_READ_TIMEOUT) == false) return (int)raCM1106.getAverage();
-  checkForReset();
+  //checkForReset();
   lastNDIRRead = millis();
   //SERIAL <<"reading\n";
-  uint16_t value = rawReadCM1106_CO2();
+  uint16_t value = rawReadCM1106_CO2(DEBUG);
   if (value == -1) return 0;
   else if ((millis() < 130L*1000) && (value == 550)) {
+    setStatus(CB_WARMUP);
     startedCO2Monitoring = false;
     return 0;
   } else {
+    setStatus(CB_STARTED);
     startedCO2Monitoring = true;
     return (int)raCM1106.getAverage();
   }
--- CubicGasSensors/CubicGasSensors.h
+++ CubicGasSensors/CubicGasSensors.h
@@ -18,15 +18,20 @@
 
 #define NDIR_READ_TIMEOUT 10000L
 
+enum CubicStatus {
+    CB_INIT, CB_WARMUP, CB_CALIBRATE, CB_STARTED
+};
+
+typedef void (*CubicStatusCb)(CubicStatus);
 // library interface description
 class CubicGasSensors
 {
   // user-accessible "public" interface
   public:
-    CubicGasSensors(uint8_t _rx, uint8_t _tx, uint16_t _eepromReset) : rx(_rx), tx(_tx), eepromReset(_eepromReset), raCM1106(2) {}
+    CubicGasSensors(uint8_t _rx, uint8_t _tx, CubicStatusCb _cb, uint16_t _eepromReset) : rx(_rx), tx(_tx), eepromReset(_eepromReset), raCM1106(2), statusCb(_cb) {}
     int getCO2(boolean dbg=false);
     void printDebugInfo();
-    int rawReadCM1106_CO2();
+    int rawReadCM1106_CO2(bool dbg=false);
     boolean hasStarted() {
         return startedCO2Monitoring;
     }
@@ -37,12 +42,16 @@ class CubicGasSensors
     uint16_t eepromReset;
     boolean startedCO2Monitoring = false;
     boolean DEBUG = false;
+    CubicStatusCb statusCb;
+    CubicStatus currentStatus = CB_INIT;
+    bool sentResetCmd = false;
 
+    void setStatus(CubicStatus newStatus);
     byte getCS(byte* buf);
     boolean validateCS(byte* resp);
     boolean sendCmd(uint8_t *cmd, uint8_t *resp);
     void co2Set400ppm();
-    void checkForReset();
+    bool checkForReset();
     void dump(uint8_t *r);
     boolean timePassed(unsigned long since, unsigned long interval) {
        return (unsigned long)(millis() - since) > interval;
